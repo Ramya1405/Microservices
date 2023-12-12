a2bcd335729210f661a98c4588aadd6363613c88
Ramya Gopala <gopal.ramaya@gmail.com>
2023-12-12 13:31:30 -0800

    initial code

--- product-service/src/main/java/com/microservices/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/productservice/ProductServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.productservice;
+package com.microservices.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
